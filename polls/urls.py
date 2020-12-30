5749643b646cb0ffe0bd3fdbf6adc8df57ab7c42
chrismedinapy <chrismedinapy@gmail.com>
2020-12-30 15:13:55 -0300

    fixbug: views.py

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from polls import views
 
 urlpatterns = [
-    path('polls', views.index, name='polls'),
+    path('', views.index, name='polls'),
     path('polls/owner', views.owner, name='owner'),
     # path('polls', views.owner, name='polls'),
     # path('<int:question_id>/', views.detail, name='detail'),
