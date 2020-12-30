401bcdf38f5a50ea56256ce99272efa38c60ff4f
chrismedinapy <chrismedinapy@gmail.com>
2020-12-30 14:32:03 -0300

    se agrego la url a polls

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,7 @@ from polls import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('owner', views.owner, name='owner'),
+    path('polls', views.owner, name='polls'),
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
