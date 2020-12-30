60eb641d148e7e68a43a32a5b6cbe06a9514d710
chrismedinapy <chrismedinapy@gmail.com>
2020-12-30 15:00:48 -0300

    se agregaron los cambios para la tearea nro 4 de django

--- polls/urls.py
+++ polls/urls.py
@@ -6,9 +6,13 @@ urlpatterns = [
     path('polls', views.index, name='polls'),
     path('polls/owner', views.owner, name='owner'),
     # path('polls', views.owner, name='polls'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # # ex: /polls/5/vote/
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
